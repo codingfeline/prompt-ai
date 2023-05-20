ab3adf62d3ed3059d103120f56c0ec921560d003
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-05-20 22:52:48 +0100

    session

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -6,7 +6,11 @@ import { usePathname, useRouter } from 'next/navigation'
 import Image from 'next/image'
 
 const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
+  const { data: session } = useSession()
+  const pathName = usePathname()
+  const router = useRouter()
   const [copied, setCopied] = useState('')
+
   const handleCopy = () => {
     setCopied(post.prompt)
     navigator.clipboard.writeText(post.prompt)
@@ -52,6 +56,22 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
       >
         {post.tag}
       </p>
+      {session?.user.id === post.creator._id && pathName === '/profile' && (
+        <div>
+          <p
+            className="font-inter text-sm green_gradient cursor-pointer"
+            onClick={handleEdit}
+          >
+            Edit
+          </p>
+          <p
+            className="font-inter text-sm orange_gradient cursor-pointer"
+            onClick={handleDelete}
+          >
+            Delete
+          </p>
+        </div>
+      )}
     </div>
   )
 }
